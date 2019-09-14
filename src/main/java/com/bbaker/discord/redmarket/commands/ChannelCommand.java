2499235db90304b0cb47796fa13e29fa5ad2ee2c
Ben Baker <Ben Baker@ben>
2019-09-13 22:19:57 -0500

    Added some validations for GMs and fixed starting permissions

--- src/main/java/com/bbaker/discord/redmarket/commands/ChannelCommand.java
+++ src/main/java/com/bbaker/discord/redmarket/commands/ChannelCommand.java
@@ -1,6 +1,10 @@
 package com.bbaker.discord.redmarket.commands;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.channel.ChannelCategory;
@@ -8,6 +12,7 @@ import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.permission.PermissionType;
 import org.javacord.api.entity.permission.Permissions;
 import org.javacord.api.entity.permission.PermissionsBuilder;
+import org.javacord.api.entity.permission.Role;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
 
@@ -19,6 +24,7 @@ public class ChannelCommand implements CommandExecutor {
     private DiscordApi api = null;
     private Optional<ChannelCategory> category = null;
     private Permissions playerPerms = null;
+    private Pattern VALID_NAME = Pattern.compile("[\\w|\\-]+");
      
     
     public ChannelCommand(DiscordApi api, String category) {
@@ -43,36 +49,63 @@ public class ChannelCommand implements CommandExecutor {
 
     @Command(aliases = {"!channel", "!c"}, description = "", usage = "!lft")
     public String onChannel(Message message) {
-        String[] args = message.getContent().split("\\s+");
-        if(args.length <= 2) {
-            return "Missing action. Try `!channel add <chanel_name>`";
-        }
-
-        if(args.length < 3) {
-            return "Missing channel name. Try `!channel add <chanel_name>`";
-        }
-
-        String channel = args[2];
-
-        Server server = message.getServer().get();
-        User creator = message.getUserAuthor().get();
+    	Server server = message.getServer().get();
+    	User creator = message.getUserAuthor().get();
+    	boolean isGm = creator.getRoles(server).stream().anyMatch(r -> r.getName().toLowerCase().equals("gm"));
+    	if(!isGm) {
+    		return "Only GMs can create channels & add users for games. Try `!gm` to give yourself the GM role";
+    	}
+    	
+    	
+    	
+         
+        List<String> args = new ArrayList<String>();
+        Collections.addAll(args, message.getContent().split("\\s+"));
+        args.remove(0); // pop the first argument, since it will always be !c
         
+        if(args.size() == 0) {
+            return "Missing action. Supported actions: `create`";
+        }
+        
+        String action = args.remove(0);
+        
+        switch (action.toLowerCase()) {
+        	case "create":
+        		return createChannel(args, creator, server);        	
+        	default:
+        		return String.format("Unknown action `%s`. Supported actions: `create`", action);
+        }
+    }
+
+	private String createChannel(List<String> args, User creator, Server server) {
+		if(args.isEmpty()) {
+            return "Missing channel name. Try `!channel create <chanel_name>`";
+        }
+
+        String channel = args.remove(0);
+
+        if(!VALID_NAME.matcher(channel).matches()) {
+        	return "Channel name can only contain alpha-numeric, underscores, and dashes";
+        }
+        
+        Role everyone = server.getEveryoneRole();        
         category.ifPresent(c -> {
             server.createTextChannelBuilder()
             	.setName(channel)
-            	.setCategory(c)
-            	.addPermissionOverwrite(creator, playerPerms)
+            	.setCategory(c)            	
+            	.addPermissionOverwrite(everyone, c.getOverwrittenPermissions(everyone))
+            	.addPermissionOverwrite(creator, playerPerms)            	
                 .create();
             
             server.createVoiceChannelBuilder()
             	.setName(channel)
             	.setCategory(c)
+            	.addPermissionOverwrite(everyone, c.getOverwrittenPermissions(everyone))
             	.addPermissionOverwrite(creator, playerPerms)
             	.create();
         });
-
-
-        return "";
-    }
+        
+        return String.format("Channel `%s` created.", channel);
+	}
 
 }
